28e2e810c126266075c6c8fc875a9dbdd4e8e672
José Victor <josevictornascimento2016@gmail.com>
2025-08-16 22:28:31 -0300

    feat: create sign in page

--- src/pages/_app/layout.tsx
+++ src/pages/_app/layout.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { Pizza } from 'lucide-react';
 
 export const Route = createFileRoute('/_app')({
   component: AppLayout,
@@ -6,8 +7,14 @@ export const Route = createFileRoute('/_app')({
 
 function AppLayout() {
   return (
-    <div>
-      <h1>Apps Layout</h1>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="border-foreground/5 bg-muted text-muted-foreground flex h-full flex-col justify-between border-r p-10">
+        <Pizza className="size-5" />
+        <span className="font-semibold">pizza.shop</span>
+      </div>
+      <footer className="text-sm">
+        Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+      </footer>
       <div>
         <Outlet />
       </div>
--- src/pages/_auth/layout.tsx
+++ src/pages/_auth/layout.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router';
+import { Pizza } from 'lucide-react';
 
 export const Route = createFileRoute('/_auth')({
   component: AuthLayout,
@@ -6,9 +7,18 @@ export const Route = createFileRoute('/_auth')({
 
 function AuthLayout() {
   return (
-    <div>
-      <h1>Auth Layout</h1>
-      <div>
+    <div className="grid min-h-screen grid-cols-2">
+      <div className="border-foreground/5 bg-muted text-muted-foreground flex h-full flex-col justify-between border-r p-10">
+        <div className="text-foreground flex items-center gap-3 text-lg">
+          <Pizza className="size-5" />
+          <span className="font-semibold">pizza.shop</span>
+        </div>
+        <footer className="text-sm">
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
+        </footer>
+      </div>
+
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
