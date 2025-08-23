570a15a881a9020f2af232cc9bbb49e1b05321c3
José Victor <josevictornascimento2016@gmail.com>
2025-08-22 21:12:29 -0300

    feat: integrate React Query and Axios for API management

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import { createRouter, RouterProvider } from '@tanstack/react-router';
 import { Toaster } from 'sonner';
 
 import { ThemeProvider } from './components/theme/theme-provider';
 import './index.css';
+import { queryClient } from './lib/react-query';
 import { NotFound } from './pages/404';
 import { routeTree } from './route-tree.gen';
 
@@ -18,7 +20,9 @@ export function App() {
   return (
     <ThemeProvider storageKey="pizzashop-theme" defaultTheme="light">
       <Toaster richColors />
-      <RouterProvider router={router} defaultNotFoundComponent={NotFound} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} defaultNotFoundComponent={NotFound} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
